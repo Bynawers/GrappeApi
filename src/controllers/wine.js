fa6b69427435c5c4740c0446bb87911f754faf8b
FRATCZAK Théo <72349226+Bynawers@users.noreply.github.com>
2023-02-21 23:26:34 +0100

    Add files via upload

--- src/controllers/wine.js
+++ src/controllers/wine.js
@@ -8,25 +8,62 @@ exports.getWine = (req, res) => {
 }
 
 exports.getAllWines = (req, res) => {
-    Wine.find()
+    const pageNumber = parseInt(req.query.page) || 0
+    const limit = parseInt(req.query.size) || 10
+    const startIndex = pageNumber * limit;
+
+    const name = req.query.name
+
+    if (name !== null) { 
+        Wine.find({name: new RegExp(name, 'i')}).sort("_id").skip(startIndex).limit(limit)
+            .then(wines => res.status(200).json(wines))
+            .catch(error => res.status(404).json({ error }));
+        return;
+    }
+    
+    Wine.find().sort("_id").skip(startIndex).limit(limit)
         .then(wines => res.status(200).json(wines))
         .catch(error => res.status(404).json({ error }));
 }
 
 exports.createWine = (req, res) => {
+    if (!req.body) { res.status(400).json({ message: "empty body" }) }
+
     delete req.body._id;
+
     const wine = new Wine({
-      ...req.body
-    });
+        name: req.body.name,
+        country: req.body.country,
+        region: req.body.region,
+        winery: req.body.winery,
+        volume: req.body.volume,
+        year: req.body.year,
+        type: req.body.type,
+        verified: req.body.verified,
+        natural: req.body.natural,
+        grape: req.body.grape,
+        rating: req.body.rating,
+        temperature_service: req.body.temperature_service,
+        characteristics: req.body.characteristics,
+        taste: req.body.taste,
+        image: `${req.protocol}://${req.get('host')}/images/${req.body.name}_${req.body.year}_${req.body.winery}.png`
+      });
+    
     wine.save()
         .then(() => res.status(201).json({ message: 'Wine saved successfully!' }))
         .catch(error => res.status(400).json({ error: error }));
 }
 
 exports.updateWine = (req, res) => {
-    Wine.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Wine updated successfully!'}))
-        .catch(error => res.status(400).json({ error }));
+    const wineObject = req.file ? {
+        ...req.body,
+        image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    
+    Wine.updateOne({ _id: req.params.id}, { ...wineObject, _id: req.params.id})
+        .then(() => res.status(200).json({message : 'Objet modifié!'}))
+        .catch(error => res.status(401).json({ error }));
 }
 
 exports.deleteWine = (req, res) => {
--- src/models/wine.js
+++ src/models/wine.js
@@ -2,18 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const Wine = new Schema({
-    vivino_id: { type: Number, require: true, unique: true },
     name: { type: String, require: true },
     country: { type: String, require: true },
     region: { type: String, require: true },
     winery: { type: String, require: true },
     volume: { type: Number, },
-    year: { type: Number },
-    count: { type: Number },
-    rate: { type: Number },
-    type: { type: String, require: true }
+    year: { type: Number, require: true },
+    rating: {
+        rate: { type: Number },
+        count: { type: Number }
+    },
+    natural: { type: Boolean },
+    type: { type: String, require: true },
+    grape: { type: String },
+    taste: {
+        acidity: { type: Number },
+        fizziness: { type: Number },
+        intensity: { type: Number },
+        sweetness: { type: Number },
+        tannin: { type: Number }
+    },
+    characteristics: [ { type: String } ],
+    temperature_service: {
+        max: { type: Number },
+        min: { type: Number }
+    },
+    image: { type: String },
+    verified: { type: Boolean },
+    type_taste: { type: String }
 }, {
     collection:'wines',
     versionKey: false
 });
+
 module.exports = mongoose.model('Wine', Wine);
